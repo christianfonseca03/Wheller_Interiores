ee343ea975e9aa2464f1d025e6f4127e50788e21
Christian Fonseca <chrisgfwheller@hotmail.com>
2023-09-13 23:34:54 -0300

    fix: new icons

--- src/app/components/Procedimento.tsx
+++ src/app/components/Procedimento.tsx
@@ -1,7 +1,7 @@
 import { CardProcedimento } from "./CardProcedimento";
 import Projeto from "../assets/plano.png";
-import Dialogo from "../assets/balao-de-fala.png";
-import Planejamento from "../assets/data-limite.png";
+import Briefing from "../assets/briefingg.png"
+import Detalhamento from "../assets/detail.png";
 import Obra from "../assets/ferramentas.png";
 
 export function Procedimento() {
@@ -12,17 +12,17 @@ export function Procedimento() {
       </h1>
       <section className="mt-12 flex flex-col justify-center items-center md:flex-row md:justify-evenly">
         <CardProcedimento
-          img={Projeto.src}
+          img={Briefing.src}
           title="BRIEFING"
           text="Antes de começarmos o projeto você preencherá um questionário para entendermos por completo as necessidades, gostos e desejos para o seu ambiente dos sonhos."
         />
         <CardProcedimento
-          img={Dialogo.src}
+          img={Projeto.src}
           title="PROJETO E APRESENTAÇÃO"
           text="Colocamos a mão na massa para transformar em realidade tudo o que antes estava no mundo das ideias! Projetamos e apresentamos para você seu ambiente com imagens realistas para a melhor compreensão do que foi projetado."
         />
         <CardProcedimento
-          img={Planejamento.src}
+          img={Detalhamento.src}
           title="DETALHAMENTO"
           text="Depois do projeto aprovado, seguimos para a etapa de detalhamento. Onde todo o projeto de marcenaria, iluminação, revestimentos, entre outros, são detalhados e especificados para que a execução ocorra conforme o projeto apresentado."
         />
