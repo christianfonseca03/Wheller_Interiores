ee618c847651b79db8fb0c8fc617c47c1f11c170
Christian Fonseca <chrisgfwheller@hotmail.com>
2023-09-18 23:45:37 -0300

    chore: living rp page

--- src/app/components/CardProcedimento.tsx
+++ src/app/components/CardProcedimento.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 export function CardProcedimento({ img, title, text }: CardProps) {
   return (
     <div className="flex flex-col justify-start items-center w-56 mb-12 md:h-80 h-fit relative">
-      <div className="flex flex-col justify-start items-center  pt-1">
+      <div className="flex flex-col justify-start items-center pt-1">
         <img src={img} alt="Etapa" className="w-14 h-14 mb-3" />
         <h1 className="mb-1 text-darkBlue-50">{title}</h1>
       </div>
--- src/app/projects/livingRP/page.tsx
+++ src/app/projects/livingRP/page.tsx
@@ -2,6 +2,14 @@ import { Header } from "@/app/components/Header";
 import Teste from "../../assets/interiores1.jpg";
 import { ProjectLayout } from "@/app/components/ProjectLayout";
 
+import FotoVertical from "./images/000_Photo - 37.jpg";
+import Foto1 from "./images/2320320.jpg";
+import Foto2 from "./images/0101.jpg";
+import Foto3 from "./images/01_Photo - 22.jpg";
+import Foto4 from "./images/01_Photo - 23.jpg";
+import Foto5 from "./images/000000.jpg";
+import { Footer } from "@/app/components/Footer";
+
 export default function LivingRP() {
   const desc = [
     "- Com um grande aproveitamento da luz natural, o living da casa RP da o tom de toda a casa, um local aconchegante e contemporâneo onde os tons claros e alguns toques de dourado se misturam com o natural da madeira e pedras utilizadas no projeto.",
@@ -12,6 +20,19 @@ export default function LivingRP() {
     <>
       <Header />
       <ProjectLayout title="Living RP" img={Teste.src} description={desc} />
+      <section className="grid grid-cols-4 gap-4 md:w-1/2 w-4/5 mx-auto justify-center mt-8 mb-28">
+        <img src={Foto1.src} className="col-span-2 h-full" alt="" />
+        <img src={Foto2.src} className="col-span-2 h-full" alt="" />
+        <img
+          src={FotoVertical.src}
+          className="row-span-2 row-start-1 col-start-3 col-span-2 h-full"
+          alt=""
+        />
+        <img src={Foto3.src} className="col-span-4 h-64 w-full" alt="" />
+        <img src={Foto4.src} className="col-span-2" alt="" />
+        <img src={Foto5.src} className="col-span-2" alt="" />
+      </section>
+      <Footer />
     </>
   );
 }
