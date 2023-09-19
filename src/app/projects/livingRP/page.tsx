9fc50d34096d441a64f5a22597f16ec6e3336991
Christian Fonseca <chrisgfwheller@hotmail.com>
2023-09-19 17:12:36 -0300

    fix: margins

--- src/app/projects/livingRP/page.tsx
+++ src/app/projects/livingRP/page.tsx
@@ -20,17 +20,17 @@ export default function LivingRP() {
     <>
       <Header />
       <ProjectLayout title="Living RP" img={Teste.src} description={desc} />
-      <section className="grid grid-cols-4 gap-4 md:w-1/2 w-4/5 mx-auto justify-center mt-8 mb-28">
-        <img src={Foto1.src} className="col-span-2 md:h-full h-32" alt="" />
-        <img src={Foto2.src} className="col-span-2 md:h-full h-32" alt="" />
+      <section className="grid grid-cols-4 gap-2 md:gap-4 md:w-1/2 w-4/5 mx-auto justify-center mt-8 md:mb-28 mb-20">
+        <img src={Foto1.src} className="col-span-2 md:h-full h-[132px]" alt="" />
+        <img src={Foto2.src} className="col-span-2 md:h-full h-[132px]" alt="" />
         <img
           src={FotoVertical.src}
           className="row-span-2 row-start-1 col-start-3 col-span-2 md:h-full h-[272px]"
           alt=""
         />
         <img src={Foto3.src} className="col-span-4 md:h-64 h-40 w-full" alt="" />
-        <img src={Foto4.src} className="col-span-2 md:h-auto h-32" alt="" />
-        <img src={Foto5.src} className="col-span-2 md:h-auto h-32" alt="" />
+        <img src={Foto4.src} className="col-span-2 md:h-auto h-[132px]" alt="" />
+        <img src={Foto5.src} className="col-span-2 md:h-auto h-[132px]" alt="" />
       </section>
       <Footer />
     </>
