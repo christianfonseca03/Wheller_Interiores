2042a9dc727c2dd63c26a5044af51a0f1e87c338
Christian Fonseca <chrisgfwheller@hotmail.com>
2023-11-07 19:14:25 -0300

    fix: image size

--- src/app/projects/quartoRP/page.tsx
+++ src/app/projects/quartoRP/page.tsx
@@ -20,17 +20,17 @@ export default function QuartoRP() {
       <section className="grid grid-cols-4 gap-2 md:gap-4 md:w-3/5 w-4/5 mx-auto justify-center mt-8 md:mb-28 mb-20">
         <img
           src={Vertical1.src}
-          className="row-span-2 row-start-1 col-start-1 col-span-2 md:h-full h-[272px] w-full"
+          className="row-span-2 row-start-1 col-start-1 col-span-2 md:h-full md:max-h-[744.688px] h-[272px] w-full"
           alt=""
         />
         <img
           src={Foto1.src}
-          className="col-span-2 md:h-full h-[132px]"
+          className="col-span-2 md:h-full md:max-h-[364.34px] h-[132px]"
           alt=""
         />
         <img
           src={Foto4.src}
-          className="col-span-2 md:h-full h-[132px]"
+          className="col-span-2 md:h-full h-[132px] md:max-h-[364.34px]"
           alt=""
         />
         <img
