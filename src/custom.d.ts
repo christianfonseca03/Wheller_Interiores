632ac12c64b13de6afc3e8b2b600b1a203b4b85b
Christian Fonseca <chrisgfwheller@hotmail.com>
2026-05-06 17:26:58 -0300

    fix: carrossel

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,1 +1,5 @@
 declare module "*.css" {}
+declare module "swiper/css" {}
+declare module "swiper/css/navigation" {}
+declare module "swiper/css/autoplay" {}
+declare module "swiper/css/pagination" {}
